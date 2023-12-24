da4cdeb12ecea6d1a7ac2fa4130048b359ac320e
ingazhur <ingazhuravlev@gmail.com>
2023-12-23 23:04:21 -0800

    update intro section

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -12,7 +12,7 @@ const IntroSection = () => {
             <img src="../../imgs/me.png"/>
         </div>
         <div className="intro-text">
-            <p>Hi! My name is Inga, and I am a junior at UC Berkeley studying Electrical Engineering & Computer Science. I am a hardware hacker curious about wearable devices, machine learning, and the future of human-computer interaction using brain-computer interfaces.</p>
+            <p>Hi! My name is Inga, and I am a senior at UC Berkeley studying Electrical Engineering & Computer Science. I am a hardware hacker curious about brain-computer interfaces and restoration of broken sensory inputs.</p>
             <div className="social-icons">
               <a href="https://github.com/ingazhur" target="_blank">
                 <FontAwesomeIcon icon={faGithub} />
